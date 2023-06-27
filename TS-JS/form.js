f992b3d3fa43d5301a7b1f5daccf7e07587882b7
DiegoJMartinezT <martineztrueba.diego@gmail.com>
2023-06-26 22:01:21 -0300

    PrimerEntrega

---
+++ TS-JS/form.js
@@ -0,0 +1,20 @@
+const user = {
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+ };
+  const form = document.querySelector("form");
+  form.addEventListener("submit", handleSubmit);
+ 
+function handleSubmit(e) {
+    e.preventDefault();
+    user.name = e.target.name.value;
+    user.name = e.target.username.value;
+    user.email = e.target.email.value;
+    user.password = e.target.password.value;
+    console.log(user);
+ }
+
+ //----------------------------------------------------------------------------------------
+ 
